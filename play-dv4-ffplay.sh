3ea17a3335c0245d054a0019a9fdecefdacb48d0
lexxai <lexxai@users.noreply.github.com>
2019-09-28 23:13:07 +0300

    Update play-dv4-ffplay.sh

--- play-dv4-ffplay.sh
+++ play-dv4-ffplay.sh
@@ -1,1 +1,3 @@
 ffplay -i video.dv4  -vf "split [maint][mainb];[maint] crop=iw:ih/2:0:0 [ftop];[mainb] crop=iw:ih/2:0:ih/2 [fbot]; [ftop]scale=iw:ih[f1]; [fbot]scale=iw:ih[f2]; [f1][f2]framepack=lines,yadif" 
+
+#https://lexxai.blogspot.com/2019/09/split-united-interlaced-video-to-frames.html
